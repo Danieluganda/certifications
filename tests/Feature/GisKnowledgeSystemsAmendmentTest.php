f046a273fe6e8eaeb2bdc67a0540935086ce567d
DANIEL UGANDA <daniel.bn1800@gmail.com>
2026-07-20 01:01:16 +0300

    Complete master certification catalogue

--- tests/Feature/GisKnowledgeSystemsAmendmentTest.php
+++ tests/Feature/GisKnowledgeSystemsAmendmentTest.php
@@ -17,26 +17,79 @@ class GisKnowledgeSystemsAmendmentTest extends TestCase
         $user = User::query()->where('email', 'learner@certpath.test')->firstOrFail();
 
         foreach ([
+            'PL-300',
+            'DP-600',
+            'AZ-104',
+            'DP-700',
+            'AZ-305',
+            'PMP',
+            'CISA',
+            'CRISC',
+            'TOGAF-FOUNDATION',
+            'TOGAF-PRACTITIONER',
+            'CISSP',
+            'PCAD',
+            'ARCGIS-PRO-FOUNDATION',
+            'ARCGIS-DEVELOPER-FOUNDATION',
+            'ARCGIS-PYTHON',
+            'ARCGIS-ONLINE-ADMIN',
+            'FME-PROFESSIONAL',
+            'ELASTIC-ENGINEER',
+            'GISP',
+        ] as $examCode) {
+            $this->assertDatabaseHas('certifications', [
+                'user_id' => $user->id,
+                'exam_code' => $examCode,
+                'track_type' => 'paid_professional',
+            ]);
+        }
+
+        foreach ([
+            'MS-APPLIED-POWER-AUTOMATE',
+            'MS-APPLIED-COPILOT-STUDIO',
+            'MS-APPLIED-AUTONOMOUS-AGENTS',
+            'AWS-EDUCATE-COMPUTE',
+            'AWS-EDUCATE-NETWORKING',
+            'AWS-EDUCATE-DATABASES',
+            'AWS-EDUCATE-CLOUD-OPERATIONS',
+            'AI-FUNDAMENTALS',
+            'IBM-PM-FUNDAMENTALS',
+            'IBM-DESIGN-THINKING',
+            'LFD121',
+            'CISCO-ETHICAL-HACKER',
+            'PMI-KICKOFF',
+            'HP-LIFE',
+            'ADBI-DPI',
             'ESRI-GIS-MOOC',
             'NASA-ARSET',
+            'EO-COLLEGE',
             'QGIS-TRAINING',
             'AGROVOC',
-            'SEMANTIC-WEB',
-            'PCAD',
-            'ELASTIC-ENGINEER',
             'GA4',
         ] as $examCode) {
             $this->assertDatabaseHas('certifications', [
                 'user_id' => $user->id,
                 'exam_code' => $examCode,
+                'track_type' => 'free_credential',
             ]);
         }
 
-        $this->assertDatabaseHas('certifications', [
-            'user_id' => $user->id,
-            'exam_code' => 'SEMANTIC-WEB',
-            'track_type' => 'skill_specialisation',
-        ]);
+        foreach ([
+            'SEMANTIC-WEB',
+            'SEARCH-IR',
+            'APACHE-SOLR',
+            'AGRI-DATASETS',
+            'BIBLIO-DATASETS',
+            'GIS-REMOTE-SENSING',
+            'POSTGIS-SPATIAL-DATA',
+            'R-ANALYTICS',
+        ] as $examCode) {
+            $this->assertDatabaseHas('certifications', [
+                'user_id' => $user->id,
+                'exam_code' => $examCode,
+                'track_type' => 'skill_specialisation',
+            ]);
+        }
 
         $this->assertDatabaseHas('projects', [
             'user_id' => $user->id,
